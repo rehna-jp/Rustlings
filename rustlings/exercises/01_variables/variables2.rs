72b5c6ba95763d4b5f31ada5c8c3fd3485c3e51e
rehna-p <jeremyrehna05@gmail.com>
2026-07-18 04:22:46 +0000

    Add Rustlings files

---
+++ rustlings/exercises/01_variables/variables2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i32 = 42;
+
+    if 10 == x {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
