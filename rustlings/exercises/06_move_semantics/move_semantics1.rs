b69704b561ce313b4b733454f265017433c30e27
rehna-p <jeremyrehna05@gmail.com>
2026-07-25 04:52:57 +0000

    finished vecs, start move_semantics

--- rustlings/exercises/06_move_semantics/move_semantics1.rs
+++ rustlings/exercises/06_move_semantics/move_semantics1.rs
@@ -1,10 +1,10 @@
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec0 = vec;
 
-    vec.push(88);
+    vec0.push(88);
 
-    vec
+    vec0
 }
 
 fn main() {
